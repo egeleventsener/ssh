a43f74b8c411aa0202d18d1f064d60851e30b8d8
egeleventsener <egeleventsener1@outlook.com>
2025-08-27 13:14:49 +0300

    fixed

--- client.c
+++ client.c
@@ -155,7 +155,7 @@ int main() {
         .host     = "192.168.0.172",   
         .port     = 22,
         .user     = "ege",             
-        .password = 211221,
+        .password = "211221",
         .pkey_path= NULL, 
         .pkey_pass= NULL,
         .dst_host = "127.0.0.1",       
